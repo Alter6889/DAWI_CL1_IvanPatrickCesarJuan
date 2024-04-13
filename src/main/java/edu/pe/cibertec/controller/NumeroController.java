375ddfabc5a03b8912c8bada8bf32612a7329c5b
CesarPeriche0701 <i201911903@cibertec.edu.pe>
2024-04-12 23:58:15 -0500

    fin proceso2

--- src/main/java/edu/pe/cibertec/controller/NumeroController.java
+++ src/main/java/edu/pe/cibertec/controller/NumeroController.java
@@ -1,4 +1,35 @@
 package edu.pe.cibertec.controller;
 
+import edu.pe.cibertec.model.NumeroModel;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
 public class NumeroController {
+
+    @GetMapping("/numero")
+public String inicioNumero(Model model){
+    model.addAttribute("numModel", new NumeroModel());
+    model.addAttribute("mostrarMensaje",false);
+    return "numero";
+
+}
+public String calcularNumero(@ModelAttribute("numModel") NumeroModel numeroModel, Model model){
+        int n1= numeroModel.getN1();
+        int n2= numeroModel.getN2();
+        int n3= numeroModel.getN3();
+        int n4= numeroModel.getN4();
+        String resultado="";
+        if(n1 != n2 && n1 != n3 && n1 !=n4 && n2 != n3 && n2 != n4 && n3 != n4){
+            if(n1>n2){
+            if(n1>n3){
+            if(n1>n4){
+            resultado= "El numero mayor es:  "+n1;}
+            }
+            }
+        }
+}
+
 }
--- src/main/java/edu/pe/cibertec/model/NumeroModel.java
+++ src/main/java/edu/pe/cibertec/model/NumeroModel.java
@@ -1,4 +1,13 @@
 package edu.pe.cibertec.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class NumeroModel {
+private int n1;
+    private int n2;
+    private int n3;
+    private int n4;
 }
