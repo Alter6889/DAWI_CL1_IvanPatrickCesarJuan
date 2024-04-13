37c4de08a4b40b447d86e1094059c800b47f2b4a
Alter6889 <etereolaider@gmail.com>
2024-04-12 23:56:28 -0500

    Ejercicio 1

--- src/main/java/edu/pe/cibertec/controller/HomeController.java
+++ src/main/java/edu/pe/cibertec/controller/HomeController.java
@@ -16,14 +16,14 @@ public class HomeController {
     @PostMapping
     public String calculutil(@ModelAttribute("Trabajador")Trabajador trabajador, Model model){
         if (trabajador.getTiempotrabajador()<1)
-            return 0.05*trabajador.getSalariotrabajador();
+            return 0.05*trabajador.getSalariotrabajador()
             else if (trabajador.getTiempotrabajador()<2)
-                return 0.07*trabajador.getSalariotrabajador();
+                return 0.07*trabajador.getSalariotrabajador()
                         else if (trabajador.getTiempotrabajador()<5)
-                            return 0.10*trabajador.getSalariotrabajador();
+                            return 0.10*trabajador.getSalariotrabajador()
                             else if (trabajador.getTiempotrabajador()<10)
-                                return 0.15*trabajador.getTiempotrabajador();
+                                return 0.15*trabajador.getTiempotrabajador()
                                 else
-                                    return trabajador.getTiempotrabajador();
+                                    return 0.20*trabajador.getTiempotrabajador()
     }
 }
